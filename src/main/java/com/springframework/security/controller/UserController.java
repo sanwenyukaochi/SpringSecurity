32317aebb7180d63c9eea1be9c6a24111c39e4f8
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-10-02 23:13:14 +0800

    docx: 重命名项目包

--- src/main/java/com/springframework/security/controller/UserController.java
+++ src/main/java/com/springframework/security/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.sanwenyukaochi.security.controller;
+package com.springframework.security.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/springframework/security/SecurityApplicationTests.java
+++ src/test/java/com/springframework/security/SecurityApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sanwenyukaochi.security;
+package com.springframework.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
