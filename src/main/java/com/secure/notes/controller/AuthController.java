40c5afd0c506136fd0186576931e17714574dc21
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-10-22 22:45:28 +0800

    feat: 获取用户名

--- src/main/java/com/secure/notes/controller/AuthController.java
+++ src/main/java/com/secure/notes/controller/AuthController.java
@@ -157,5 +157,9 @@ public class AuthController {
         return ResponseEntity.ok().body(response);
     }
 
+    @GetMapping("/username")
+    public String currentUserName(@AuthenticationPrincipal UserDetails userDetails) {
+        return (userDetails != null) ? userDetails.getUsername() : "";
+    }
 
 }
