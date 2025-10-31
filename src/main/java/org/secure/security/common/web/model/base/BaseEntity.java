626729756284ad816e897929888d45c798df9d7f
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-01 00:57:42 +0800

    feat: 新增 User

---
+++ src/main/java/org/secure/security/common/web/model/base/BaseEntity.java
@@ -0,0 +1,27 @@
+package org.secure.security.common.web.model.base;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@Schema(name = "BaseEntity", title = "基础信息字段", description = "基础信息字段")
+public abstract class BaseEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @Comment("主键ID")
+    @Schema(name = "id", title = "主键ID")
+    private Long id;
+
+}
