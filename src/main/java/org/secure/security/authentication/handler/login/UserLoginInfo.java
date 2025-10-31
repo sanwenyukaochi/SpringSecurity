293739df12cda2964627f414f4dbe7a01029d170
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-01 00:57:42 +0800

    feat: 新增 User

--- src/main/java/org/secure/security/authentication/handler/login/UserLoginInfo.java
+++ src/main/java/org/secure/security/authentication/handler/login/UserLoginInfo.java
@@ -1,6 +1,11 @@
 package org.secure.security.authentication.handler.login;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * 用户信息登陆后的信息，会序列化到Jwt的payload
@@ -10,15 +15,45 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserLoginInfo {
+public class UserLoginInfo implements UserDetails {
 
     private String sessionId; // 会话id，全局唯一
     private Long userId;
     private String username;
     private String password;
     private String phone;
-    private String nickname; // 昵称
-    private String roleId;
+    private String email;
+    private Boolean accountNonLocked;
+    private Boolean accountNonExpired;
+    private Boolean credentialsNonExpired;
+    private Boolean enabled;
+    private String twoFactorSecret;
+    private Boolean twoFactorEnabled;
+    private String signUpMethod;
+    private Long expiredTime; // JWT过期时间
 
-    private Long expiredTime; // 过期时间
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
