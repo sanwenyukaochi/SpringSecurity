eed8524a825bdae52445937ea43e1fc3c084e9e7
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-01 01:49:28 +0800

    style: 清理代码

--- src/main/java/org/secure/security/authentication/handler/login/sms/SmsAuthentication.java
+++ src/main/java/org/secure/security/authentication/handler/login/sms/SmsAuthentication.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.secure.security.authentication.handler.login.UserLoginInfo;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
 
 @Setter
 @Getter
@@ -13,8 +16,18 @@ public class SmsAuthentication extends AbstractAuthenticationToken {
     private String smsCode;
     private UserLoginInfo currentUser;
 
-    public SmsAuthentication() {
+    public SmsAuthentication(String phone, String smsCode, Boolean authenticated) {
+        this.phone = phone;
+        this.smsCode = smsCode;
         super(null); // 权限，用不上，直接null
+        super.setAuthenticated(authenticated);
+    }
+
+    public SmsAuthentication(UserLoginInfo currentUser, Boolean authenticated,
+                             Collection<? extends GrantedAuthority> authorities) {
+        this.currentUser = currentUser;
+        super(authorities);
+        super.setAuthenticated(authenticated);
     }
 
     @Override
