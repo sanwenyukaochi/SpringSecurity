614f173379560fa41e612bde9d08d3b4bf367a44
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-11-13 23:41:11 +0800

    fix: 修复 CustomAuthenticationExceptionHandler 异常

--- src/main/java/com/secure/security/authentication/handler/exception/CustomAuthenticationExceptionHandler.java
+++ src/main/java/com/secure/security/authentication/handler/exception/CustomAuthenticationExceptionHandler.java
@@ -32,13 +32,8 @@ public class CustomAuthenticationExceptionHandler implements AuthenticationEntry
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException e) throws IOException, ServletException {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        if (!HttpMethod.POST.name().equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpStatus.METHOD_NOT_ALLOWED.value());
-            objectMapper.writeValue(response.getOutputStream(), Result.builder().code(ResponseCodeConstants.METHOD_NOT_ALLOWED).message("请求方法不支持" + request.getMethod()).build());
-        } else {
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            log.warn("登录异常：msg={}", e.getMessage(), e);
-            objectMapper.writeValue(response.getOutputStream(), Result.builder().code(ResponseCodeConstants.LOGIN_FAIL).message("认证失败").build());
-        }
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        log.warn("登录异常：msg={}", e.getMessage(), e);
+        objectMapper.writeValue(response.getOutputStream(), Result.builder().code(ResponseCodeConstants.LOGIN_FAIL).message("认证失败").build());
     }
 }
