e6d45629ac955a59c98d345aab877924c3ef1cda
sanwenyukaochi <sanwenyukaochi@outlook.com>
2026-03-10 13:21:02 +0800

    clear: OAuth2AuthorizationResponseUtils

--- src/main/java/com/spring/security/authentication/handler/auth/utils/OAuth2AuthorizationResponseUtils.java
+++ src/main/java/com/spring/security/authentication/handler/auth/utils/OAuth2AuthorizationResponseUtils.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2004-present the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.spring.security.authentication.handler.auth.utils;
 
 import java.util.Map;
@@ -30,10 +14,8 @@ public final class OAuth2AuthorizationResponseUtils {
     public static MultiValueMap<String, String> toMultiMap(Map<String, String[]> map) {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>(map.size());
         map.forEach((key, values) -> {
-            if (values.length > 0) {
-                for (String value : values) {
-                    params.add(key, value);
-                }
+            for (String value : values) {
+                params.add(key, value);
             }
         });
         return params;
@@ -65,13 +47,11 @@ public final class OAuth2AuthorizationResponseUtils {
         }
         String errorDescription = request.getFirst(OAuth2ParameterNames.ERROR_DESCRIPTION);
         String errorUri = request.getFirst(OAuth2ParameterNames.ERROR_URI);
-        // @formatter:off
         return OAuth2AuthorizationResponse.error(errorCode)
                 .redirectUri(redirectUri)
                 .errorDescription(errorDescription)
                 .errorUri(errorUri)
                 .state(state)
                 .build();
-        // @formatter:on
     }
 }
